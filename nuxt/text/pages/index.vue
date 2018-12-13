7f9d06ae34f0e6264daa68bf7317958f5f1662ff
e <280828574@qq.com>
2018-12-13 19:36:00 +0800

    新增 vue-cli3.0 体验

---
+++ nuxt/text/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="done">
+    <li>{{ todos }}</li>
+    <li>
+      <input
+        placeholder="What needs to be done?"
+        @keyup.enter="addTodo">
+    </li>
+  </ul>
+</template>
+
+<script>
+  import { mapMutations } from 'vuex'
+
+  export default {
+    computed: {
+      todos () { return this.$store.state.todos.list }
+    },
+    methods: {
+      addTodo (e) {
+        this.$store.commit('todos/add', e.target.value);
+        e.target.value = ''
+      },
+      ...mapMutations({
+        toggle: 'todos/toggle'
+      })
+    }
+  }
+</script>
+
+<style>
+  .done {
+    text-decoration: line-through;
+  }
+</style>
