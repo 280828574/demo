6c273656901aef464ca8ea553ceb84cc952d8aee
e <280828574@qq.com>
2019-01-23 15:00:20 +0800

    新增Vue-Cli3.0关闭打包生成map文件配置

--- vue/test/vue.config.js
+++ vue/test/vue.config.js
@@ -19,5 +19,6 @@ module.exports = {
     chainWebpack: config => {
         config.resolve.alias
             .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
-    }
+    },
+    productionSourceMap: false, // 关闭打包map文件
 }
