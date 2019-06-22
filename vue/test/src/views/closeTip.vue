4c85cff606f260e285d527d402afa095c503e37e
hanleng <280828574@qq.com>
2019-06-22 13:26:10 +0800

    111

---
+++ vue/test/src/views/closeTip.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+    created() {
+  window.addEventListener('beforeunload', e => this.beforeunloadFn(e))
+},
+   methods: {
+  beforeunloadFn(e) {
+    console.log('刷新或关闭')
+    // ...
+  }
+},
+destroyed() {
+  window.removeEventListener('beforeunload', e => this.beforeunloadFn(e))
+}
+}
+</script>
+
